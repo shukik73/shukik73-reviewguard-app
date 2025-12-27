ae1b0a6d42812e45398aa46042e75b95f66902e5
shukik73 <k.shuki@gmail.com>
2025-12-27 04:08:13 +0000

    Remove development artifacts from tracking

--- attached_assets/database_1764998096941.js
+++
@@ -1,8 +0,0 @@
-import pg from 'pg';
-
-const { Pool } = pg;
-
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
-});
